955a43293f1ebb242f745e5af65744cc9abf6e36
Lenard <bebobhd@gmail.com>
2025-11-19 15:32:30 +0000

    attempt to rename TrustProxies to get around name-in-use error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Middleware\HandleInertiaRequests;
-use App\Http\Middleware\TrustProxies;
+use App\Http\Middleware\TrustProxiesLocal;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append: [
-            TrustProxies::class,
+            TrustProxiesLocal::class,
             HandleInertiaRequests::class,
         ]);
     })
