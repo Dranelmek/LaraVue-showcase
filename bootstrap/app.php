859d2842802977d22ffca74518922d55feaf8eeb
Lenard <bebobhd@gmail.com>
2025-11-19 18:12:20 +0000

    attempt to use new Laravel syntax for middleware usage in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->appendToGlobal(TrustProxies::class);
+        $middleware->append([
+            TrustProxies::class,
+        ]);
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
