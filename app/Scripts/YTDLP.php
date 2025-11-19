56d6a344a3002a033137bd7db566ba9578a19e29
Lenard <bebobhd@gmail.com>
2025-11-19 23:06:47 +0000

    yt-dlp debugging and cleaner YTDLP class

--- app/Scripts/YTDLP.php
+++ app/Scripts/YTDLP.php
@@ -5,25 +5,36 @@ namespace App\Scripts;
 class YTDLP
 {
     // contains methods that return execution strings for yt-dlp commands
-    
+
     public static function update()
     {
-        // simple update command
-        $output = "yt-dlp -U 2>&1";
-        return $output;
+	    // simple update command
+        return "yt-dlp -U 2>&1";
     }
 
     public static function mp3Download($url)
     {
-        $output = 'yt-dlp -x --audio-format mp3 -o "' . storage_path('app/output/%(title)s.%(ext)s') . '" "' . escapeshellarg($url) . '" 2>&1';
-        return $output;
+        $out = storage_path("app/output/%(title)s.%(ext)s");
+
+        return "yt-dlp -x --audio-format mp3 -o " .
+            escapeshellarg($out) . " " .
+            escapeshellarg($url) .
+            " 2>&1";
     }
 
     public static function mp4Download($url, $quality = null)
     {
-        $outPath = '"' . str_replace('\\', '/', storage_path('app/temp/%(title)s.%(ext)s')) . '"';
-        $qualityOption = "yt-dlp -f \"bv*[height<=" . rtrim($quality, 'p') . "]+ba/b\" -o \"" . $outPath . '" "' . escapeshellarg($url) . '" 2>&1';
-        $noQualityOption = 'yt-dlp -o "' . $outPath . '" "' . escapeshellarg($url) . '" 2>&1';
-        return $quality ? $qualityOption : $noQualityOption;
+        $out = storage_path("app/temp/%(title)s.%(ext)s");
+
+        $cmd = "yt-dlp ";
+
+        if ($quality) {
+            $height = rtrim($quality, "p");
+            $cmd .= "-f " . escapeshellarg("bv*[height<={$height}]+ba/b") . " ";
+        }
+
+        $cmd .= "-o " . escapeshellarg($out) . " " . escapeshellarg($url) . " 2>&1";
+
+        return $cmd;
     }
 }
